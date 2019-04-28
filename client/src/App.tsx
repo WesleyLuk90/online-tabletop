f7f4941ebcafd64fc92fdd3f0cd4fb855c03b495
Wesley Luk <wesleyluk@gmail.com>
2019-04-28 13:11:56 -0700

    Simple nav

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,39 @@
-import React from "react";
-import "normalize.css/normalize.css";
+import {
+    Alignment,
+    Button,
+    Classes,
+    Navbar,
+    NavbarDivider,
+    NavbarGroup,
+    NavbarHeading
+} from "@blueprintjs/core";
 import "@blueprintjs/core/lib/css/blueprint.css";
+import "normalize.css/normalize.css";
+import React from "react";
 import "./App.css";
-import { Button } from "@blueprintjs/core";
 
 export class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <Button intent="success" text="button content" />
+                <Navbar>
+                    <NavbarGroup>
+                        <NavbarHeading>Online Tabletop</NavbarHeading>
+                        <NavbarDivider />
+                        <Button
+                            className={Classes.MINIMAL}
+                            icon="home"
+                            text="Home"
+                        />
+                    </NavbarGroup>
+                    <NavbarGroup align={Alignment.RIGHT}>
+                        <Button
+                            className={Classes.MINIMAL}
+                            icon="user"
+                            text="Profile"
+                        />
+                    </NavbarGroup>
+                </Navbar>
             </div>
         );
     }
