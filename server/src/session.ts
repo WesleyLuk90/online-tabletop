b09a9c7be3fefd631119f3bf46a9a27ce0030cba
Wesley Luk <wesleyluk@gmail.com>
2019-04-30 21:08:30 -0700

    Persist sessions

--- server/src/session.ts
+++ server/src/session.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import session from "express-session";
+import SessionFileStore from "session-file-store";
+
+const Store = SessionFileStore(session);
 
 export async function initializeSession(app: express.Express) {
     app.use(
         session({
             secret: process.env.SESSION_SECRET,
             resave: false,
-            saveUninitialized: false
+            saveUninitialized: false,
+            store: new Store({ ttl: 60 * 60 * 24 * 7 })
         })
     );
 }
