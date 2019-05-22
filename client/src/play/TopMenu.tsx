b5774cdb0ff494484fe35766f2fce1c246b0eee3
Wesley Luk <wesleyluk@gmail.com>
2019-05-21 18:42:43 -0700

    WIP

--- client/src/play/TopMenu.tsx
+++ client/src/play/TopMenu.tsx
@@ -1,9 +1,8 @@
 import { Button, Classes, Popover } from "@blueprintjs/core";
 import { Campaign } from "protocol/lib/Campaign";
-import { newId } from "protocol/lib/Id";
 import { Message } from "protocol/lib/Messages";
-import { Scene } from "protocol/lib/Scene";
 import * as React from "react";
+import { SceneMenu } from "./SceneMenu";
 
 interface Props {
     campaign: Campaign;
@@ -11,51 +10,11 @@ interface Props {
 }
 
 export class TopMenu extends React.Component<Props> {
-    addScene = () => {
-        this.props.onMessage({
-            type: "update-scene",
-            id: newId(),
-            scene: {
-                id: newId(),
-                name: "New Scene",
-                tokens: []
-            }
-        });
-    };
-
-    toScene = (scene: Scene) => {
-        this.props.onMessage({
-            type: "update-campaign",
-            id: newId(),
-            campaign: {
-                ...this.props.campaign
-            }
-        });
-    };
-
-    renderScenes() {
-        return (
-            <div className="scene-menu">
-                {this.props.campaign.scenes.map(s => (
-                    <div key={s.id}>
-                        <Button className={Classes.MINIMAL} icon="flag" />{" "}
-                        {s.name}
-                    </div>
-                ))}
-                <Button
-                    className={Classes.MINIMAL}
-                    text="Add Scene"
-                    onClick={this.addScene}
-                />
-            </div>
-        );
-    }
-
     render() {
         return (
             <div className="top-menu">
                 <Popover
-                    content={this.renderScenes()}
+                    content={<SceneMenu {...this.props} />}
                     minimal
                     position="bottom-left"
                 >
