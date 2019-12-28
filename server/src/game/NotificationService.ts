003580240206a963f9f5dbc5f55433958d2c6fbb
Wesley Luk <wesleyluk@gmail.com>
2019-12-27 18:49:13 -0800

    WIP

---
+++ server/src/game/NotificationService.ts
@@ -0,0 +1,7 @@
+import { Campaign } from "protocol/src/Campaign";
+import { Scene } from "protocol/src/Scene";
+
+export class NotificationService {
+    campaignUpdated(campaign: Campaign) {}
+    sceneUpdated(scene: Scene) {}
+}
