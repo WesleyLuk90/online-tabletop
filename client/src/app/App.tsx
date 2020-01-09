658561133f8d1e4f008dcccd1931e52816b3e338
Wesley Luk <wesleyluk@gmail.com>
2020-01-08 21:56:41 -0800

    Use portal for modal

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,6 +1,7 @@
 import "normalize.css/normalize.css";
 import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
+import { ModalContainer } from "../common/Modal";
 import { EditCampaignPage } from "../games/EditCampaignPage";
 import { PlayPage } from "../play/PlayPage";
 import "./App.css";
@@ -11,25 +12,27 @@ import { Routes } from "./Routes";
 export class App extends React.Component {
     render() {
         return (
-            <Router>
-                <Navigation />
-                <Route path="/" exact component={Dashboard} />
-                <Route
-                    path={Routes.createCampaign()}
-                    exact
-                    component={EditCampaignPage}
-                />
-                <Route
-                    path={Routes.editCampaign(":id")}
-                    exact
-                    component={EditCampaignPage}
-                />
-                <Route
-                    path={Routes.playGame(":id")}
-                    exact
-                    component={PlayPage}
-                />
-            </Router>
+            <ModalContainer>
+                <Router>
+                    <Navigation />
+                    <Route path="/" exact component={Dashboard} />
+                    <Route
+                        path={Routes.createCampaign()}
+                        exact
+                        component={EditCampaignPage}
+                    />
+                    <Route
+                        path={Routes.editCampaign(":id")}
+                        exact
+                        component={EditCampaignPage}
+                    />
+                    <Route
+                        path={Routes.playGame(":id")}
+                        exact
+                        component={PlayPage}
+                    />
+                </Router>
+            </ModalContainer>
         );
     }
 }
--- client/src/common/Modal.tsx
+++ client/src/common/Modal.tsx
@@ -1,9 +1,23 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useCallback, useContext, useState } from "react";
+import { createPortal } from "react-dom";
 import { BemBuilder } from "../util/BemBuilder";
 import { Button } from "./Button";
 import "./Modal.css";
 
 const BEM = new BemBuilder("modal");
+
+const ModalContext = React.createContext<HTMLDivElement | null>(null);
+
+export function ModalContainer({ children }: { children: ReactNode }) {
+    const [element, setElement] = useState<HTMLDivElement | null>(null);
+    const setRef = useCallback(setElement, []);
+    return (
+        <ModalContext.Provider value={element}>
+            <div ref={setRef}></div>
+            {children}
+        </ModalContext.Provider>
+    );
+}
 
 export function Modal({
     visible,
@@ -14,14 +28,21 @@ export function Modal({
     children: ReactNode;
     title: ReactNode;
 }) {
-    return (
-        <div className={BEM.block("visible", visible !== false)}>
-            <div className={BEM.element("content")}>
-                <div className="modal__header">{title}</div>
-                {children}
-            </div>
-        </div>
-    );
+    const modalContext = useContext(ModalContext);
+    if (modalContext != null) {
+        return createPortal(
+            <div className={BEM.block("visible", visible !== false)}>
+                <div className={BEM.element("content")}>
+                    <div className="modal__header">{title}</div>
+                    {children}
+                </div>
+            </div>,
+            modalContext
+        );
+    } else {
+        console.warn("No modal container found");
+        return null;
+    }
 }
 
 export function ModalForm({
