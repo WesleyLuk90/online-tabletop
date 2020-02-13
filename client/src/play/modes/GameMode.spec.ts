39a2d0bd91d2a21ba48e8003b02dd1471549dadd
Wesley Luk <wesleyluk@gmail.com>
2020-02-12 20:22:36 -0800

    validate editors

--- client/src/play/modes/GameMode.spec.ts
+++ client/src/play/modes/GameMode.spec.ts
@@ -1,7 +1,11 @@
+import { assertExhaustive } from "../../util/Exaustive";
+import { Component, Control } from "./Editor";
 import { AttributeType, EntityType } from "./GameMode";
 import { GameModes } from "./GameModes";
 
 describe("GameMode", () => {
+    const entityTypeValidators = [validateNameAttribute, validateEditor];
+
     function validateNameAttribute(entity: EntityType) {
         expect(entity.attributes.map(a => a.id)).toContain(
             entity.nameAttributeID
@@ -11,7 +15,38 @@ describe("GameMode", () => {
         ).toBe(AttributeType.Text);
     }
 
+    function validateEditor(entity: EntityType) {
+        function validateControl(control: Control) {
+            expect(entity.attributes.map(a => a.id)).toContain(
+                control.attributeID
+            );
+        }
+        function validateComponent(component: Component) {
+            switch (component.type) {
+                case "control":
+                    return validateControl(component);
+                case "control-row":
+                    return component.controls.forEach(validateControl);
+                case "row":
+                    return component.columns.forEach(c =>
+                        c.components.forEach(validateComponent)
+                    );
+                case "section":
+                    return component.components.forEach(validateComponent);
+                default:
+                    assertExhaustive(component);
+            }
+        }
+        entity.editor.pages.forEach(page => {
+            page.components.forEach(validateComponent);
+        });
+    }
+
     it("should validate", () => {
-        GameModes.forEach(g => g.entityTypes.forEach(validateNameAttribute));
+        GameModes.forEach(g =>
+            g.entityTypes.forEach(type =>
+                entityTypeValidators.forEach(validator => validator(type))
+            )
+        );
     });
 });
