ec0d46743a615fd8960a2a77b8728ad4f810f421
Wesley Luk <wesleyluk@gmail.com>
2020-08-16 10:12:08 -0700

    Setter

--- server/src/storage/BaseModel.ts
+++ server/src/storage/BaseModel.ts
@@ -2,13 +2,10 @@ import { Field } from "./BaseSchema";
 import { Row } from "./BaseStore";
 
 export abstract class BaseModel {
-    constructor(public row: Row) {}
-
-    setter<T>(field: Field<T>): (value: T) => this {
-        const self = this;
-        return (value) => {
-            this.row.values.set(field, value);
-            return self;
+    static setter<T>(field: Field<T>) {
+        return (prototype: object, name: string) => {
+            console.log(prototype, name);
         };
     }
+    constructor(public row: Row) {}
 }
--- server/test/storage/DatabaseIntegration.spec.ts
+++ server/test/storage/DatabaseIntegration.spec.ts
@@ -1,4 +1,4 @@
-import { BaseModel } from "src/storage/BaseModel";
+import { BaseModel } from "server/src/storage/BaseModel";
 import { BaseSchema } from "../../src/storage/BaseSchema";
 import { BaseStore, Results, Row } from "../../src/storage/BaseStore";
 import { Database } from "../../src/storage/Database";
@@ -20,6 +20,12 @@ class TestModel extends BaseModel {
     constructor(row: Row = new Row()) {
         super(row);
     }
+
+    @BaseModel.setter(TestSchema.id)
+    setID: (s: string) => this;
+
+    @BaseModel.setter(TestSchema.name)
+    setName: (s: string) => this;
 }
 
 class TestStore extends BaseStore<TestModel> {
@@ -30,22 +36,13 @@ class TestStore extends BaseStore<TestModel> {
 
 describe("DatabaseIntegration", () => {
     const fixture = new DatabaseFixture();
+
     it("should work", async () => {
         const store = new TestStore(fixture.db);
 
-        await store.create(
-            Row.of([
-                [TestSchema.id, "foo"],
-                [TestSchema.name, "bar"],
-            ])
-        );
+        await store.create(new TestModel().setID("foo").setName("bar"));
         expect(await store.find(new Query({ from: TestSchema }))).toEqual(
-            new Results([
-                Row.of([
-                    [TestSchema.id, "foo"],
-                    [TestSchema.name, "bar"],
-                ]),
-            ])
+            new Results([new TestModel().setID("foo").setName("bar")])
         );
     });
 });
