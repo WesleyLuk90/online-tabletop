480663622a884adf2168843a8333d5b6783ccf85
Wesley Luk <wesleyluk@gmail.com>
2020-08-16 10:21:49 -0700

    Postgres

--- server/test/storage/DatabaseFixture.ts
+++ server/test/storage/DatabaseFixture.ts
@@ -1,14 +1,16 @@
 import { uuid } from "engine/utils/Uuid";
+import { AppModule } from "server/src/AppModule";
 import { Database } from "../../src/storage/Database";
 import { Migrator } from "../../src/storage/Migrator";
 
 export class DatabaseFixture {
+    module = new AppModule();
     db = new Database(
-        "localhost",
-        5432,
-        "postgres",
-        "devpassword",
-        "test" + uuid()
+        this.module.dbHost(),
+        this.module.dbPort(),
+        this.module.dbUser(),
+        this.module.dbPass(),
+        this.module.dbSchema() + uuid()
     );
     private migrator = new Migrator(this.db);
 
