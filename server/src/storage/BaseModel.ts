58c9440350603e7ba1cc48ca39edd001204a07e0
Wesley Luk <wesleyluk@gmail.com>
2020-08-16 10:13:37 -0700

    Fix

--- server/src/storage/BaseModel.ts
+++ server/src/storage/BaseModel.ts
@@ -3,9 +3,13 @@ import { Row } from "./BaseStore";
 
 export abstract class BaseModel {
     static setter<T>(field: Field<T>) {
-        return (prototype: object, name: string) => {
-            console.log(prototype, name);
+        return (prototype: any, name: string) => {
+            prototype[name] = function (value: T) {
+                (this as BaseModel).row.values.set(field, value);
+                return this;
+            };
         };
     }
+
     constructor(public row: Row) {}
 }
